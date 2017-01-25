c23216d8aeddc9ebbb185f118f5518a69e3b9e15
Alvin Yuen <alvinyuen628@gmail.com>
2017-01-25 17:38:08 -0600

    passport login working

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const SET_LOGIN_USER = 'SET_LOGIN_USER';
+import { SET_LOGIN_USER } from '../constants' ;
+
 
 export const setLoginUser = (user) => ({
   type: SET_LOGIN_USER,
@@ -9,8 +10,14 @@ export const setLoginUser = (user) => ({
 
 
 export const createUser = (firstName, lastName, email, password) => dispatch => {
-  console.log('email & pass:', {first_name: firstName, last_name: lastName, email: email, password: password });
-  axios.post('/api/user/', {first_name: firstName, last_name: lastName, email: email, password: password })
+  axios.post('/api/user/', {first_name: firstName, last_name: lastName, email, password })
     .then(res => console.log(res.data))
     .catch(err => console.error(err));
 };
+
+export const loginUser = (email, password) => dispatch => {
+  axios.post('/api/auth/', { email: email, password: password })
+    .then(res => console.log(res.data))
+    .catch(err => console.error(err));
+};
+
