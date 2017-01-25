b1360a909b9c60f0ca00095a16db896a881d9ad2
spencewine <sdgoodwine@gmail.com>
2017-01-25 17:06:25 -0600

    grabbing updated master

--- client/reducers/boardReducer.js
+++ client/reducers/boardReducer.js
@@ -1,10 +1,11 @@
-import RECEIVE_BOARD from '../constants';
+import {RECEIVE_BOARD} from '../constants';
 
 const initialState = {selectedBoard: {}, allBoards: []};
 
 export default function(state = initialState, action) {
   const newState = Object.assign({}, state);
 
+  console.log("IN BOARD REDUCER", RECEIVE_BOARD)
   switch (action.type) {
   case RECEIVE_BOARD:
     newState.selectedBoard = action.board;
@@ -15,8 +16,10 @@ export default function(state = initialState, action) {
       // break;
 
   default:
+    console.log('DEFAULT STATE')
     return state;
   }
+  console.log("NEW STATE", newState)
 
   return newState;
 }
