65e689946cf0df9d6fd4f6c2e818bbbf232a879e
Joe Stephens <Joe Stephens>
2017-01-25 16:07:08 -0600

    fixed index

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import store from './store';
 import {Route, Router, browserHistory} from 'react-router';
+
+//containers
 import BoardContainer from './containers/BoardContainer';
-
-
+import CreateBoardContainer from './containers/CreateBoardContainer';
 import Signup from './containers/SignupContainer';
 
-function Index() {
-  return <h1>Capstone</h1>;
-}
+//components
+import Index from './components/Index';
 
 export default function Routes() {
   return (
     <Router history={browserHistory}>
-      <Route path="/" component={Index}/>
-      <Route path="/signup" component={Signup}>
+      <Route path="/" component={Index}>
+      <Route path="/signup" component={Signup} />
+      <Route path="/myboards" component={CreateBoardContainer} />
       </Route>
       <Route path='/boards/:boardId' component={BoardContainer}/>
     </Router>
