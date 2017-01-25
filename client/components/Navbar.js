07c3829fbbc733c0564cdffc3b9c71776b798f27
Joe Stephens <Joe Stephens>
2017-01-25 15:08:45 -0600

    another test for tab

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,14 +3,15 @@ import {Link} from 'react-router';
 
 const Navbar = (props) => {
 	return (
+
 	<div>
     <nav className="navbar navbar navbar-static-top" role="navigation">
       <div className="container">
         <div className="navbar-header">
-						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
               aria-expanded={props.aria} aria-controls="navbar" onClick={() => {
-                props.expandNav();
-              }}>
+	props.expandNav();
+}}>
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
