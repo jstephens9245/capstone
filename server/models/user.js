33edaf0d0d4323c32aefd2b9a031a231d99c8c1a
Alvin Yuen <alvinyuen628@gmail.com>
2017-01-25 11:00:36 -0600

    bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt-nodejs');
 const db = require('./db');
 
 const User = db.define('user', {
@@ -8,6 +9,46 @@ const User = db.define('user', {
 	last_name : Sequelize.STRING,
 	email     : Sequelize.STRING,
 	password  : Sequelize.TEXT
+}, {
+	instanceMethods: {
+		hashPassword: () => {
+			return new Promise((resolve, reject) => {
+				bcrypt.genSalt(4, (err, salt) => {
+					if (err) {
+						return reject(err);
+					}
+					bcrypt.hash(this.password, salt, null, (hashErr, hash) => {
+						if (hashErr) {
+							return reject(err);
+						}
+						this.password = hash;
+						resolve();
+					});
+				});
+			});
+		},
+		checkPassword: (password) => {
+			return new Promise((resolve, reject) => {
+				bcrypt.compare(password, this.password, (err, matched) => {
+					if (err) {
+						return reject(err);
+					}
+					resolve(matched);
+				});
+			});
+		}
+	},
+	hooks: {
+		beforeCreate: function(user) {
+			user.hashPassword();
+		},
+		beforeUpdate: function(user) {
+			if (!user.changed('password')) {
+				return;
+			}
+			user.hashPassword();
+		}
+	}
 });
 
 module.exports = User;
