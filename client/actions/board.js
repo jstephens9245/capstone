fc154b9d53c3ecbca6a09b78bef160aeb7c71f04
spencewine <sdgoodwine@gmail.com>
2017-01-26 11:51:00 -0600

    boardcontainer changes

--- client/actions/board.js
+++ client/actions/board.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {RECEIVE_BOARD} from '../constants';
+import {RECEIVE_BOARD_NOTES} from '../constants';
 
 
 export const receiveBoard = (board) => {
@@ -9,10 +10,13 @@ export const receiveBoard = (board) => {
   }
 };
 
-// export const receiveBoards = (boards) => ({
-//   type: RECEIVE_BOARDS,
-//   boards
-// });
+export const receiveBoardNotes = (notes) => {
+  return {
+  type: RECEIVE_BOARD_NOTES,
+  notes
+  }
+};
+
 
 export const getBoard = (boardId) => (dispatch) => {
 
@@ -22,3 +26,14 @@ export const getBoard = (boardId) => (dispatch) => {
       dispatch(receiveBoard(board))
     });
 };
+
+
+export const getBoardNotes = (boardId) => (dispatch) =>{
+  console.log('IN GBN')
+  axios.get('/api/notes/', {params: {board_id: boardId}})
+    .then(res => res.data)
+    .then(boardNotes => {
+      dispatch(receiveBoardNotes(notes))
+    })
+
+}
