2bfb329b6f432dd1d5f55ab24779d44ee64c6acf
Joe Stephens <jstephens9245@gmail.com>
2017-01-27 13:35:47 -0600

    fixed constant.js

--- client/constants.js
+++ client/constants.js
@@ -5,8 +5,6 @@ export const CLEAR_STORE = 'CLEAR_STORE';
 export const RECEIVE_BOARD = 'RECEIVE_BOARD';
 export const RECEIVE_BOARDS = 'RECEIVE_BOARDS';
 export const ADD_NEW_BOARD = 'ADD_NEW_BOARD';
-export const SET_LOGIN_USER = 'SET_LOGIN_USER';
-export const CLEAR_STORE = 'CLEAR_STORE';
 
 /* NOTE CONSTANTS */
 export const RECEIVE_NOTE = 'RECEIVE_NOTE';
