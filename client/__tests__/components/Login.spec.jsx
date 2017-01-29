2182b8fc8a0d46630e13786be540bebb1313822b
Alvin Yuen <alvinyuen628@gmail.com>
2017-01-28 19:38:07 -0600

    Signup, Login tests

---
+++ client/__tests__/components/Login.spec.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import chai, { expect } from 'chai';
+import {shallow} from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+import { spy } from 'sinon';
+
+import Login from '../../components/Login';
+
+chai.use(chaiEnzyme());
+
+describe ('<Login />', () => {
+  let loginWrapper, sendLoginSpy, handleInputSpy, loginFormSpy;
+
+  beforeEach(('create spy to pass in component', () => {
+    sendLoginSpy = spy();
+    handleInputSpy = spy();
+    loginFormSpy = spy();
+  }));
+
+  beforeEach(() => {
+    loginWrapper = shallow(
+      <Login
+      changeForm={sendLoginSpy}
+      handleInput={handleInputSpy}
+      loginForm={loginFormSpy} />);
+  });
+
+  it('signup link in login page should work', () => {
+    loginWrapper.find('.signup-link').simulate('click');
+    expect(sendLoginSpy.called).to.be.true;
+  });
+
+  it('should handle input changes in login component', () => {
+    const email = 'alvin@alvin.com';
+    const loginInput = loginWrapper.find('#login-email-input-field');
+    loginInput.simulate('change', {target: {value: email}});
+    expect(handleInputSpy.called).to.be.true;
+  });
+
+  it('should handle submit when user logins in as a user', () => {
+    const loginSubmit = loginWrapper.find('.login-submit-button');
+    loginSubmit.simulate('click');
+    expect(loginFormSpy.called).to.be.true;
+  });
+});
---
+++ client/__tests__/components/Signup.spec.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import chai, { expect } from 'chai';
+import {shallow} from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+import { spy } from 'sinon';
+
+import Signup from '../../components/Signup';
+
+chai.use(chaiEnzyme());
+
+describe('<Signup />', () => {
+  let signupWrapper, loginLinkSpy, handleInputSpy, submitFormSpy;
+
+  beforeEach(('create spies to pass in component', () => {
+    loginLinkSpy = spy();
+    handleInputSpy = spy();
+    submitFormSpy = spy();
+  }));
+
+  beforeEach(() => {
+    signupWrapper = shallow(
+      <Signup
+    changeForm={loginLinkSpy}
+    handleInput={handleInputSpy}
+    submitForm={submitFormSpy}/>);
+  });
+
+  it('login link in signup page should work', () => {
+    signupWrapper.find('.login-link').simulate('click');
+    expect(loginLinkSpy.called).to.be.true;
+  });
+
+  it('should handle inputs change in signup component', () => {
+    const firstNameInput = 'Alvin';
+    const signupInput = signupWrapper.find('#signup-firstname-input-field');
+    signupInput.simulate('change', {target: {value: firstNameInput}});
+    expect(handleInputSpy.called).to.be.true;
+    // expect(handleInputSpy.calledWith('Alvin')).to.be.true;
+    // not working ?
+  });
+
+  it('should handle submit when user signups as new user', () => {
+    const signupSubmit = signupWrapper.find('.signup-submit-button');
+    signupSubmit.simulate('click');
+    expect(submitFormSpy.called).to.be.true;
+  });
+});
+
