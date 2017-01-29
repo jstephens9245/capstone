dc44cd42fc2a10777d7736719e03ba6b4137b7e2
Alvin Yuen <alvinyuen628@gmail.com>
2017-01-28 19:38:07 -0600

    Signup, Login tests

--- client/components/Login.js
+++ client/components/Login.js
@@ -9,6 +9,7 @@ const Login = (props) => {
         <div className="form-group">
           <input type="email" name="email" placeholder="E-mail"
           className="signup-input-field"
+          id="login-email-input-field"
           onChange={(e) => { props.handleInput(e); }} />
         </div>
         <div className="form-group">
@@ -17,11 +18,12 @@ const Login = (props) => {
           name="password"
           placeholder="password"
           className="signup-input-field"
+          id="login-password-input-field"
           onChange={(e) => { props.handleInput(e); }} />
         </div>
         <button className="login-submit-button" type="submit" onClick={(e) => { props.loginForm(e); }}> Login </button>
         <div className="signup-link-container" >
-        Don't have an account? <label><a onClick={() => { props.changeForm('signup'); }}> Signup</a> </label>
+        Don't have an account? <label><a className="signup-link" onClick={() => { props.changeForm('signup'); }}> Signup</a> </label>
         </div>
       </form>
       );
--- client/components/Signup.js
+++ client/components/Signup.js
@@ -9,21 +9,25 @@ const Signup = (props) => {
         <div className="form-group">
           <input type="text" name="firstName" placeholder="First name"
           className="input signup-input-field"
+          id="signup-firstname-input-field"
           onChange={(e) => { props.handleInput(e); }} />
         </div>
         <div className="form-group">
           <input type="text" name="lastName" placeholder="Last name"
           className="signup-input-field"
+          id="signup-lastname-input-field"
           onChange={(e) => { props.handleInput(e); }} />
         </div>
         <div className="form-group">
           <input type="email" name="email" placeholder="Email"
           className="signup-input-field"
+          id="signup-email-input-field"
           onChange={(e) => { props.handleInput(e); }} />
         </div>
         <div className="form-group">
           <input type="password" name="password" placeholder="Password"
           className="signup-input-field"
+          id="signup-password-input-field"
           onChange={(e) => { props.handleInput(e); }} />
         </div>
         <div className="">
@@ -31,7 +35,7 @@ const Signup = (props) => {
         </div>
         <button className="signup-submit-button" type="submit" onClick={(e) => { props.submitForm(e); }}> SIGN UP </button>
          <div className="login-link-container" >
-        Already have an account? <label><a onClick={() => { props.changeForm('login'); }}> Login</a> </label>
+        Already have an account? <label><a className="login-link" onClick={() => { props.changeForm('login'); }}> Login</a> </label>
         </div>
       </form>
       );
