f56ed36c12a8e91784cf06621ecbdf535f3bd6a5
Hal Carleton <hello@halleycarleton.com>
2017-01-29 19:26:03 -0600

    Adding custom note color functionality and complementary text color

--- client/components/Note.js
+++ client/components/Note.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import bindHandlers from '../utils/bindHandlers';
+import Color from 'color';
 
 const initState = {
-  focused: false
+  focused: false,
+  color  : Color.rgb([ 237, 208, 13 ])
 };
 
 export default class Note extends Component {
@@ -36,10 +38,15 @@ export default class Note extends Component {
   }
 
   render() {
+    const noteStyle = {
+      backgroundColor: this.state.color.rgb().string(),
+      color          : this.state.color.rotate(180).rgb().string()
+    };
     return (
       <div
         className={`c-note ${this.state.focused ? 'c-note--focused' : ''}`}
-        onClick={this.clickHandler}>
+        onClick={this.clickHandler}
+        style={noteStyle}>
         <div className="c-note__inner">
           <div className="c-note__content">{this.props.content}</div>
           { this.props.editable &&
