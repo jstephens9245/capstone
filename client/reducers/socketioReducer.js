f1611708824b37de972aabe6899ce1f43766706e
Alvin Yuen <alvinyuen628@gmail.com>
2017-01-30 14:11:33 -0600

    Update socketioReducer.js

--- client/reducers/socketioReducer.js
+++ client/reducers/socketioReducer.js
@@ -1,6 +1,6 @@
 import socketClient from 'socket.io-client';
 const io =  socketClient;
-import _ from 'lodash';
+import isEmpty from 'lodash/isEmpty'
 
 import {CONNECT, EMIT, ADD_LISTENER, REMOVE_LISTENER } from '../constants';
 
@@ -14,17 +14,17 @@ export default function(socket = initialState, action) {
     newSocket = io(`http://localhost:3030/${action.namespace}`);
     break;
   case EMIT:
-    if (!_.isEmpty(newSocket)) {
+    if (!isEmpty(newSocket)) {
       newSocket.emit(action.eventName, action.payload);
     }
     break;
   case ADD_LISTENER:
-    if (!_.isEmpty(newSocket)) {
+    if (!isEmpty(newSocket)) {
       newSocket.on(action.eventName, action.method);
     }
     break;
   case REMOVE_LISTENER:
-    if (!_.isEmpty(newSocket)) {
+    if (!isEmpty(newSocket)) {
       newSocket.removeListener(action.eventName, action.method);
     }
     break;
