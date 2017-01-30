a56692e39f17d774639d683feaf6e14e41f9489b
Hal Carleton <hello@halleycarleton.com>
2017-01-29 18:30:34 -0600

    Creating bindHandlers utility

---
+++ client/utils/bindHandlers.js
@@ -0,0 +1,12 @@
+export default function bindHandlers(ctxt, ...handlers) {
+  handlers.forEach((handler) => {
+    if (typeof handler === 'string') handler = ctxt[handler];
+    const hdlrName = handler.name;
+    console.log(hdlrName);
+    try {
+      ctxt[hdlrName] = handler.bind(ctxt);
+    } catch (e) {
+      console.warn(`Could not bind the handler ${hdlrName}.`);
+    }
+  });
+}
