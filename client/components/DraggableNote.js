306b35d124f878c19d743c8c75daed1cbe854240
Hal Carleton <hello@halleycarleton.com>
2017-02-01 12:58:36 -0600

    Update DraggableNote.js

--- client/components/DraggableNote.js
+++ client/components/DraggableNote.js
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent} from 'react';
+import React, {PureComponent} from 'react';
 import { DragSource } from 'react-dnd';
 import { getEmptyImage } from 'react-dnd-html5-backend';
 import {NOTE} from '../constants';
